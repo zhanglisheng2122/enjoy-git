4a678a4214615eafef9145fdd2ee50aec2d29277
zhanglisheng <zhanglisheng@okay.cn>
2019-04-04 14:13:37 +0800

    'v1.1'

---
+++ src/main/java/Test.java
@@ -0,0 +1,14 @@
+import cn.enjoy.vo.Product;
+
+/**
+ * @description:
+ * @author: zhanglisheng
+ * @create: 2019-04-04 11:07
+ **/
+public class Test {
+    private Product product;
+
+    public static void main(String[] args) {
+
+    }
+}
